02afcd8f663b02bec3f0d85d5186524239797d33
o0Ignition0o <jeremy.lempereur@gmail.com>
2019-11-23 15:43:23 +0100

    Added intersection tests.

--- src/git.rs
+++ src/git.rs
@@ -1,5 +1,9 @@
 use std::process::Command;
 
+pub struct Parser {
+    verbose: bool,
+}
+
 #[derive(Debug)]
 pub struct Section {
     pub file_name: String,
@@ -47,66 +51,69 @@ impl SectionBuilder {
     }
 }
 
-pub fn get_diff_sections(
-    target_branch: &str,
-    verbose: bool,
-) -> Result<Vec<Section>, crate::error::Error> {
-    diff(target_branch, verbose).map(|diff| sections(&diff))
-}
-
-fn diff(target: &str, verbose: bool) -> Result<String, crate::error::Error> {
-    let cmd_output = Command::new("git")
-        .args(&["diff", "-u", target])
-        .output()
-        .expect("Could not run git command.");
-    if verbose {
-        println!("{}", String::from_utf8(cmd_output.stdout.clone())?);
+impl Parser {
+    pub fn new(verbose: bool) -> Self {
+        Self { verbose }
     }
-    if cmd_output.status.success() {
-        Ok(String::from_utf8(cmd_output.stdout)?)
-    } else {
-        Err(String::from_utf8(cmd_output.stderr)?.into())
-    }
-}
 
-fn sections(git_diff: &str) -> Vec<Section> {
-    let mut sections = Vec::new();
-    let mut file_name = "";
-    for l in git_diff.lines() {
-        // Add or edit a file
-        // +++ b/Cargo.lock
-        if l.starts_with("+++") {
-            // TODO: do something less ugly with the bounds and indexing
-            file_name = l[l.find('/').unwrap() + 1..].into();
+    pub fn get_sections(&self, target_branch: &str) -> Result<Vec<Section>, crate::error::Error> {
+        self.diff(target_branch).map(|diff| self.sections(&diff))
+    }
+
+    fn diff(&self, target: &str) -> Result<String, crate::error::Error> {
+        let cmd_output = Command::new("git")
+            .args(&["diff", "-u", target])
+            .output()
+            .expect("Could not run git command.");
+        if self.verbose {
+            println!("{}", String::from_utf8(cmd_output.stdout.clone())?);
         }
-        // Actual diff lines
-        // @@ -33,6 +33,9 @@ version = "0.1.0"
-        if l.starts_with("@@") {
-            // For now, we will focus on the added lines.
-            // @@ and space
-            let after_ats = &l[3..];
-            // space and @@
-            let before_second_ats_index = &after_ats.find("@@").unwrap() - 1;
-            let diff_lines = &after_ats[..before_second_ats_index];
-            // -33,6 +33,9
-            let (_, a) = diff_lines.split_at(diff_lines.find(' ').unwrap());
-            let added = a.trim();
+        if !cmd_output.status.success() {
+            Err(String::from_utf8(cmd_output.stderr)?.into())
+        } else {
+            Ok(String::from_utf8(cmd_output.stdout)?)
+        }
+    }
 
-            let (added_start, added_span) = if let Some(index) = added[1..].find(',') {
-                let (a, b) = added[1..].split_at(index);
-                (a, &b[1..])
-            } else {
-                (added, "")
-            };
-            let min_line_start = added_start.parse::<i32>().unwrap();
-            let mut current_section = SectionBuilder::new();
-            current_section.file_name(file_name.to_string());
-            current_section.line_start(min_line_start);
-            current_section.line_end(min_line_start + added_span.parse::<i32>().unwrap_or(1));
-            if let Some(s) = current_section.build() {
-                sections.push(s);
+    fn sections(&self, git_diff: &str) -> Vec<Section> {
+        let mut sections = Vec::new();
+        let mut file_name = "";
+        for l in git_diff.lines() {
+            // Add or edit a file
+            // +++ b/Cargo.lock
+            if l.starts_with("+++") {
+                // TODO: do something less ugly with the bounds and indexing
+                file_name = l[l.find('/').unwrap() + 1..].into();
+            }
+            // Actual diff lines
+            // @@ -33,6 +33,9 @@ version = "0.1.0"
+            if l.starts_with("@@") {
+                // For now, we will focus on the added lines.
+                // @@ and space
+                let after_ats = &l[3..];
+                // space and @@
+                let before_second_ats_index = &after_ats.find("@@").unwrap() - 1;
+                let diff_lines = &after_ats[..before_second_ats_index];
+                // -33,6 +33,9
+                let (_, a) = diff_lines.split_at(diff_lines.find(' ').unwrap());
+                let added = a.trim();
+
+                let (added_start, added_span) = if let Some(index) = added[1..].find(',') {
+                    let (a, b) = added[1..].split_at(index);
+                    (a, &b[1..])
+                } else {
+                    (added, "")
+                };
+                let min_line_start = added_start.parse::<i32>().unwrap();
+                let mut current_section = SectionBuilder::new();
+                current_section.file_name(file_name.to_string());
+                current_section.line_start(min_line_start);
+                current_section.line_end(min_line_start + added_span.parse::<i32>().unwrap_or(1));
+                if let Some(s) = current_section.build() {
+                    sections.push(s);
+                }
             }
         }
+        sections
     }
-    sections
 }
--- src/main.rs
+++ src/main.rs
@@ -4,37 +4,16 @@ use clap::{App, Arg};
 mod clippy;
 mod error;
 mod git;
+mod intersections;
 
-fn lines_in_range(span: &clippy::Span, diff: &git::Section) -> bool {
-    span.line_start >= diff.line_start && span.line_start <= diff.line_end
-        || span.line_end >= diff.line_start && span.line_end <= diff.line_end
-}
-
-fn get_lints_from_diff(
-    lints: &[clippy::Lint],
-    diffs: &[git::Section],
-    _verbose: bool,
-) -> Vec<clippy::Lint> {
-    let mut lints_in_diff = Vec::new();
-    for diff in diffs {
-        let diff_lints = lints.iter().filter(|lint| {
-            if let Some(m) = &lint.message {
-                for s in &m.spans {
-                    // Git diff paths and clippy paths don't get along too well on Windows...
-                    if s.file_name.replace("\\", "/") == diff.file_name {
-                        return lines_in_range(&s, &diff);
-                    }
-                }
-                false
-            } else {
-                false
+fn display_warnings(warnings: &[clippy::Lint]) {
+    for w in warnings {
+        if let Some(m) = &w.message {
+            for l in m.rendered.split('\n') {
+                println!("{}", l);
             }
-        });
-        for l in diff_lints {
-            lints_in_diff.push(l.clone());
         }
     }
-    lints_in_diff
 }
 
 fn main() -> Result<(), error::Error> {
@@ -62,24 +41,22 @@ fn main() -> Result<(), error::Error> {
     let target_branch = matches.value_of("branch").unwrap_or("master");
 
     println!("Getting diff against target {}", target_branch);
-    let diff_sections = git::get_diff_sections(target_branch, verbose)?;
+    let git_parser = git::Parser::new(verbose);
+    let diff_sections = git_parser.get_sections(target_branch)?;
     println!("Running clippy");
     let clippy_lints = clippy::get_clippy_lints(verbose)?;
 
-    let warnings_caused_by_diff = get_lints_from_diff(&clippy_lints, &diff_sections, verbose);
+    let warnings_caused_by_diff =
+        intersections::get_lints_from_diff(&clippy_lints, &diff_sections, verbose);
     if warnings_caused_by_diff.is_empty() {
         println!("No warnings raised by clippy::pedantic in your diff, you're good to go!");
         Ok(())
     } else {
-        let total_warnings = warnings_caused_by_diff.len();
-        for w in warnings_caused_by_diff {
-            if let Some(m) = w.message {
-                for l in m.rendered.split('\n') {
-                    println!("{}", l);
-                }
-            }
-        }
-        println!("Clippy::pedantic found {} warnings", total_warnings);
+        display_warnings(&warnings_caused_by_diff);
+        println!(
+            "Clippy::pedantic found {} warnings",
+            warnings_caused_by_diff.len()
+        );
         Err(error::Error::NotClean)
     }
 }
